fca569413c4f10e63fde54d84ae3d687d32fe869
Chris <chris@DESKTOP-0EGUENT.localdomain>
2021-06-04 23:46:57 +0200

    C3 update/nicht vollständig

--- src/main/java/de/freerider/repository/CustomerRepository.java
+++ src/main/java/de/freerider/repository/CustomerRepository.java
@@ -1,12 +1,104 @@
 package de.freerider.repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Component;
+
+import de.freerider.model.Customer;
 
 
 @Component
-class CustomerRepository { //implements CrudRepository<Customer, String> {
+public class CustomerRepository implements CrudRepository<Customer, String> {
 	//
 	private final IDGenerator idGen = new IDGenerator( "C", IDGenerator.IDTYPE.NUM, 6 );
+	HashMap<String,Customer> customers = new HashMap<>();
+
+	@Override
+	public <S extends Customer> S save(S entity) {
+		if(entity.getId() == null || entity.getId().equals("")) {
+			String id;
+			while(true) {
+				id = idGen.nextId();
+				if(customers.containsKey(id)==false) 
+				break;
+			}
+			entity.setId(id);
+				}
+		customers.put(entity.getId(), entity);
+		return entity;
+	}
+	
+
+	@Override
+	public <S extends Customer> Iterable<S> saveAll(Iterable<S> entities) {
+		for (Customer customer: entities)
+			save(customer);
+		return entities;
+	}
+
+	@Override
+	public Optional<Customer> findById(String id) {
+		Customer customer = customers.get(id);
+		return Optional.of(customer);
+	}
+
+	@Override
+	public boolean existsById(String id) {
+		if(customers.containsKey(id))
+			return true;
+		return false;
+	}
+
+	@Override
+	public Iterable<Customer> findAll() {
+		
+		return customers.values();
+	}
+
+	@Override
+	public Iterable<Customer> findAllById(Iterable<String> ids) {
+		ArrayList<Customer> list = new ArrayList<>();
+		for (String key : ids)
+			findById(key);
+		return list;
+	}
+
+	@Override
+	public long count() {
+		return customers.size();
+	}
+
+	@Override
+	public void deleteById(String id) {
+		// TODO Auto-generated method stub
+		customers.remove(id);
+	}
+
+	@Override
+	public void delete(Customer entity) {
+		customers.remove(entity.getId(), entity);
+	}
+
+	@Override
+	public void deleteAllById(Iterable<? extends String> ids) {
+		// TODO Auto-generated method stub
+		for(String key : ids)
+			deleteById(key);
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends Customer> entities) {
+		for(Customer customer : entities)
+			delete(customer);
+	}
+
+	@Override
+	public void deleteAll() {
+		customers.clear();	
+	}
 
 
 	
---
+++ src/test/java/de/freerider/CustomerTests.java
@@ -0,0 +1,67 @@
+package de.freerider;
+
+import java.util.ArrayList;
+
+import de.freerider.model.*;
+import de.freerider.repository.*;
+
+class CustomerTests {
+	
+	public static void main (String[] args) {
+	Customer muller= new Customer("Muller", "Chris","chrismuller3@gmx.de" );
+	Customer holly= new Customer("Holly", "Pony","ponyholly6@gmx.de" );
+	Customer eva= new Customer("Eva", "Erika","erieva@gmx.de" );
+	Customer schmidt= new Customer("Schmidt", "Horst","schmidt7@gmail.de" );
+	Customer grobian= new Customer("Grobian", "Gerard","grobger@hotmail.com");
+	
+	
+	ArrayList<Customer> liste = new ArrayList<>();
+	liste.add(muller);
+	liste.add(holly);
+	liste.add(eva);
+	liste.add(schmidt);
+	liste.add(grobian);
+	CustomerRepository carSharing = new CustomerRepository();
+	
+	//save test
+		carSharing.save(muller);
+		carSharing.save(holly);
+	//count test
+	System.out.println("count funktioniert + Zwei Objekte sollte drinne sein:  "+ carSharing.count());
+	
+	// deletebyId test
+	carSharing.deleteById(muller.getId());
+	
+	System.out.println("Ein Objekt "+ carSharing.count());
+	
+//	Delete test
+	carSharing.delete(holly);
+	
+	System.out.println("Sollte 0 sein: " +carSharing.count());
+	// SaveAll Test
+	carSharing.saveAll(liste);
+	System.out.println("5 Objekte sollten drinne sein:  "+ carSharing.count());
+
+	
+	//DeleteAll Test
+	carSharing.deleteAll();
+	System.out.println("Sollte 0 ausgeben:"+ carSharing.count() );
+	
+	carSharing.saveAll(liste);
+	//DelteById test
+	carSharing.deleteById(eva.getId());
+	System.out.println("Sollte 4 ausgeben:"+ carSharing.count() );
+	//DeleteAllById Test
+	ArrayList<String> idTest = new ArrayList<>();
+	idTest.add(muller.getId());
+	idTest.add(grobian.getId());
+	carSharing.deleteAllById(idTest);
+	System.out.println("Sollte 2 ausgeben:"+ carSharing.count() );
+	
+	//deleteAll(Iterable<? extends Customer> entities) test
+	carSharing.deleteAll(liste);
+	System.out.println("Sollte 0 ausgeben:"+ carSharing.count() );
+}
+	
+	
+}
